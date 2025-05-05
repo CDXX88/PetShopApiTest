422945505988744bc2c136a04596b5a77e45ff5f
CDXX88 <59917101+CDXX88@users.noreply.github.com>
2025-05-05 22:39:37 +0500

    Update base_test.py

--- config/base_test.py
+++ config/base_test.py
@@ -1,8 +1,10 @@
 from services.store.api_methods import StoreAPI
 from services.pet.api_methods import PetAPI
+from services.user.api_methods import UserAPI
 
 class BaseTest:
 
     def setup_method(self):
         self.store_api = StoreAPI()
         self.pet_api = PetAPI()
+        self.user_api = UserAPI()
