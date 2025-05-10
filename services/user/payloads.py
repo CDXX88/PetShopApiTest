11cc53ed0a4eb9dd888ae8345edf739918d90a4d
CDXX88 <cdxx@protonmail.com>
2025-05-10 18:49:46 +0300

    Added and described all methods for user service. Added all payloads for user methods.

--- services/user/payloads.py
+++ services/user/payloads.py
@@ -1,2 +1,31 @@
+from faker import Faker
+import random
+import json
+
+fake = Faker()
+
 class Payloads:
-    ...
+
+    def user_payload(self):
+        return {
+            "id": random.randint(1, 1000),
+            "username": fake.user_name(),
+            "firstName": fake.first_name(),
+            "lastName": fake.last_name(),
+            "email": fake.email(),
+            "password": fake.password(length=10),
+            "phone": fake.phone_number(),
+            "userStatus": random.choice([0, 1])
+        }
+
+    def user_list_payload(self):
+        counter = random.randint(1, 10)
+        users_array = []
+        for i in range(1, counter):
+            users_array.append(self.user_payload())
+        return users_array
+
+# if __name__ == '__main__':
+#     payload = Payloads()
+#     list_ = payload.user_list_payload()
+#     print(json.dumps(list_, indent=2))
