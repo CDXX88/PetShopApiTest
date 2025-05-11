5a43601d0ef668c32b5f74480b7f62836e52bd92
CDXX88 <cdxx@protonmail.com>
2025-05-11 06:26:23 +0300

    Added tests

--- services/user/api_methods.py
+++ services/user/api_methods.py
@@ -7,16 +7,19 @@ from services.user.urls import Urls
 class UserAPI:
 
     def __init__(self):
-        self.urls = Urls()
-        self.payloads = Payloads()
+        self._urls = Urls()
+        self._payloads = Payloads()
 
     def post_user(self):
-        url = f'{self.urls.base_url}{self.urls.user}'
-        payload = self.payloads.user_payload()
+        url = f'{self._urls.base_url}{self._urls.user}'
+        payload = self._payloads.user_payload()
         username = payload.get('username')
         password = payload.get('password')
         response = requests.post(url, json=payload)
-        response_data = response.json()
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code,
@@ -25,77 +28,83 @@ class UserAPI:
         }
 
     def post_user_create_with_list(self):
-        url = f'{self.urls.base_url}{self.urls.user_with_array}'
-        payload = self.payloads.user_list_payload()
+        url = f'{self._urls.base_url}{self._urls.user_with_array}'
+        payload = self._payloads.user_list_payload()
         response = requests.post(url, json=payload)
-        response_data = response.json()
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
     def get_user_login(self, username, password):
-        url = f'{self.urls.base_url}{self.urls.user_login}'
+        url = f'{self._urls.base_url}{self._urls.user_login}'
         params = {
             'username': username,
             'password': password
         }
-        response = requests.post(url, params=params)
-        response_data = response.json()
+        response = requests.get(url, params=params)
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
     def get_user_by_user_name(self, username):
-        url = f'{self.urls.base_url}{self.urls.user_by_username}'
-        params = {
-            'username': username
-        }
-        response = requests.get(url, params=params)
-        response_data = response.json()
+        url = f'{self._urls.base_url}{self._urls.user_by_username}{username}'
+        response = requests.get(url)
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
     def put_user_by_user_name(self, username):
-        url = f'{self.urls.base_url}{self.urls.user_by_username}'
-        params = {
-            'username': username
-        }
-        response = requests.get(url, params=params)
-        response_data = response.json()
+        url = f'{self._urls.base_url}{self._urls.user_by_username}{username}'
+        response = requests.put(url, json=self._payloads.user_payload())
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
     def delete_user_by_user_name(self, username):
-        url = f'{self.urls.base_url}{self.urls.user_by_username}'
-        params = {
-            'username': username
-        }
-        response = requests.get(url, params=params)
-        response_data = response.json()
+        url = f'{self._urls.base_url}{self._urls.user_by_username}{username}'
+        response = requests.delete(url)
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
     def get_user_logout(self):
-        url = f'{self.urls.base_url}{self.urls.user_logout}'
-        response = requests.post(url)
-        response_data = response.json()
+        url = f'{self._urls.base_url}{self._urls.user_logout}'
+        response = requests.get(url)
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
         return {
             'body': response_data,
             'status_code': response.status_code
         }
 
-
-if __name__ == '__main__':
-    api = UserAPI()
-    method = api.post_user()
-    print(method['username'])
-    print(method['password'])
-
+# if __name__ == '__main__':
+#     api = UserAPI()
+#     method = api.get_user_login(username='user', password='passs')
+#     print(method['status_code'])
--- services/user/urls.py
+++ services/user/urls.py
@@ -3,7 +3,7 @@ class Urls:
     base_url = 'https://petstore.swagger.io/v2'
 
     user = '/user' ## create user
-    user_with_array = '/user/createWithArray' ## create list of users with given input array
+    user_with_array = '/user/createWithList' ## create list of users with given input array
     user_login = '/user/login' ## logs user into the system
     user_logout = '/user/logout' ## logs out current logged-in user session
     user_by_username = '/user/' ## add {username} in the end of url, used in delete_user, update_user, get_user_by_username
