a807e72f682b4fef834a4e10a9dd80d6275955f8
CDXX88 <cdxx@protonmail.com>
2025-06-10 08:19:13 +0300

    Added environment variables docker and remote. Remote is default. Also action.yml also changed for new settings. Test will run in two invironments.

--- services/pet/urls.py
+++ services/pet/urls.py
@@ -1,6 +1,12 @@
+import os
 class Urls:
 
-    base_url = 'http://localhost:8080/api/v3'
+    stage = os.getenv('STAGE', 'REMOTE').upper()
+
+    if stage == 'DOCKER':
+        base_url = 'http://localhost:8080/api/v3'
+    elif stage == 'REMOTE':
+        base_url = 'http://petstore.swagger.io/v2/pet'
 
     pet = '/pet' ## add a new pet to the store and update an existing pet, used in put_pet and post_pet
     pet_id = '/pet/' ## used in delete, update, get_by_id
--- services/store/urls.py
+++ services/store/urls.py
@@ -1,6 +1,12 @@
+import os
 class Urls:
 
-    base_url = 'http://localhost:8080/api/v3'
+    stage = os.getenv('STAGE', 'REMOTE').upper()
+
+    if stage == 'DOCKER':
+        base_url = 'http://localhost:8080/api/v3'
+    elif stage == 'REMOTE':
+        base_url = 'http://petstore.swagger.io/v2/pet'
 
     inventory = '/store/inventory/' ## Returns pet inventories by status
     order = '/store/order/' ## Place an order for a pet
